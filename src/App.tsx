b20d520999b02e4647f2ebf56955ed30abd36125
Abdul Owhab <owhab.cse@gmail.com>
2023-09-19 01:25:03 +0600

    completed mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
-import ColorModeSwitch from "./components/ColorModeSwitch";
 
 const App = () => {
   return (
@@ -11,7 +10,7 @@ const App = () => {
       }}
     >
       <GridItem area="nav">
-        <Navbar /> <ColorModeSwitch />
+        <Navbar />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" bg="red">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/game_explorer.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 const Navbar = () => {
   return (
     <HStack>
       <Image src={logo} boxSize="60px" />
-      <Text>Game Explorer</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
