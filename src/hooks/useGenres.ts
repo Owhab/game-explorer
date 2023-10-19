cd73db91f4157b1a2c09399c2ba5fba9067f2088
Abdul Owhab <owhab.cse@gmail.com>
2023-10-19 08:52:12 +0600

    added local data as initial data for genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import apiClient from "../services/api-client";
 import { FetchResponse } from "./useData";
+import genres from "../data/genres";
 export interface Genre {
   id: number;
   name: string;
@@ -12,6 +13,8 @@ const useGenres = () =>
     queryKey: ["genres"],
     queryFn: () =>
       apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    initialData: { count: genres.length, results: genres },
   });
 
 export default useGenres;
